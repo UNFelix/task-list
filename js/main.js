56bcfb86740d7b17430b36206f3694095b84a911
unfelix <shevko.evheniy@gmail.com>
2020-05-01 20:22:52 +0300

    sync articles to localstorage

--- js/main.js
+++ js/main.js
@@ -1,12 +1,14 @@
-let articles = [
-  "my article1",
-  "articles text",
-  "Lorem ipsum dolor",
-  "articles author",
-  "bla bla",
-  "bla text",
-  "Lorem ipsum",
-];
+// let articles = [
+//   "my article1",
+//   "articles text",
+//   "Lorem ipsum dolor",
+//   "articles author",
+//   "bla bla",
+//   "bla text",
+//   "Lorem ipsum",
+// ];
+
+let articles = JSON.parse(localStorage.articles || '["item1", "item2"]');
 
 let html = "";
 for (let article of articles) {
@@ -19,14 +21,14 @@ ul.innerHTML = html;
 let closebtns = document.getElementsByClassName("close");
 
 for (let i = 0; i < closebtns.length; ++i) {
-  closebtns[i].addEventListener("click", function () {
-    this.parentElement.remove();
-  });
+  closebtns[i].addEventListener("click", handleDel);
 }
 
 const deleteBtn = document.querySelector(".delete");
 deleteBtn.addEventListener("click", function () {
   while (ul.children.length > 0) ul.children[0].remove();
+  articles.length = 0;
+  localStorage.articles = JSON.stringify(articles);
 });
 
 function addListItem(str) {
@@ -40,21 +42,31 @@ function addListItem(str) {
   span.innerHTML = "&times;";
   li.append(span);
   span.classList.add("close");
-  span.addEventListener("click", function () {
-    this.parentElement.remove();
-  });
+  span.addEventListener("click", handleDel);
 }
 
 const input = document.querySelector(".input");
 const addBtn = document.querySelector(".addBtn");
-addBtn.addEventListener("click", function () {
-  addListItem(input.value);
-  input.value = "";
-});
+addBtn.addEventListener("click", handleAdd);
 input.addEventListener("keyup", function (event) {
-  if (event.key == "Enter") {
-    addListItem(input.value);
-    input.value = "";
-    // addBtn.click();
-  }
+  if (event.key == "Enter") handleAdd();
 });
+
+function handleAdd() {
+  addListItem(input.value);
+  articles.push(input.value);
+  localStorage.articles = JSON.stringify(articles);
+  input.value = "";
+}
+
+function handleDel() {
+  const li = this.parentElement;
+  for (let i = 0; i < ul.children.length; ++i) {
+    if (ul.children[i] == li) {
+      articles.splice(i, 1);
+      localStorage.articles = JSON.stringify(articles);
+      break;
+    }
+  }
+  li.remove();
+}
